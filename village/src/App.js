e35cf83a025a9d051e6120b2a2890a0af515904e
yannafaith <yannafaith97@gmail.com>
2019-01-25 09:44:37 -0700

    add Route paths in App

--- village/src/App.js
+++ village/src/App.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
@@ -28,8 +29,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm />
-        <Smurfs smurfs={this.state.smurfs} />
+      
+        <Route exact path='/' 
+        render={props => (<SmurfForm {...props} />)}
+        />
+
+        <Route exact path='/' 
+        render={props => (<Smurfs {...props} smurfs={this.state.smurfs} />)}
+        />
+
+
       </div>
     );
   }
